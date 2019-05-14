d159acb731eebd67567907e7f9a582bf7a152e0b
Haoyun Zhu <zhuh12@rpi.edu>
2019-05-14 13:39:18 -0400

    .

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import os
 import sys
+import random
 from classicGrid import classicGrid
 
 if __name__ == "__main__":
-    a = classicGrid()
-    while a.winner == None:
-        print(a)
-        cmd = input("Put \""+str(a.next)+"\" at: ")
-        cmd = [int (i)  for i in cmd.strip().split()]
-        if len(cmd) != 2: continue
-        a.put(cmd[0],cmd[1])  
-    if a.winner != -1:
-        print(a.winner,"wins!")
-    else: 
-        print("Game tied.")
-    a.save2("save1")
+    count = 0
+    while count <=1000:
+        count+=1
+        a = classicGrid()
+        while a.winner == None:
+            a.put(0,random.randint(0,8))  
+        if a.winner != -1:
+            print(count,":",a.winner,"wins!")
+        else: 
+            print(count,":","Game tied.")
+        a.save2("save1")
 
 
     # a = classicGrid(id = "d3a8f31d-ae9e-471e-8b6a-fa5880333d3f")
