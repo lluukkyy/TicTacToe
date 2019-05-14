299abe881768c8c2219de06c9a54c5e16d42ac94
Haoyun Zhu <zhuh12@rpi.edu>
2019-05-14 13:33:25 -0400

    .

--- classicGrid.py
+++ classicGrid.py
@@ -1,55 +1,44 @@
 import os
-import pickle
-import uuid
+
 
 class classicGrid:
-    def __init__(self, id = None, first = "o"):
-        if id != None:
-            idString = str(id)
-            file = open("files/"+idString,"rb")
-            self.grid = pickle.load(file)
-            file.close()
-            self.id = uuid.UUID(id)
-        else:
-            self.grid = [ [".",".","."],[".",".","."],[".",".","."] ]
-            self.id = uuid.uuid4()
+    def __init__(self, first = 0):
+        self.grid = [-1] * 9
         self.winner = None
-        self.next = "o"
+        self.next = 0
 
     def put(self, row, col):
         if self.winner != None: return
-        if self.grid[row][col] != ".": return
-        self.grid[row][col] = self.next
-        self.next = "x" if self.next == "o" else "o"
+        index = row * 3 + col
+        if self.grid[index] != -1: return
+        self.grid[index] = self.next
+        self.next = 1 if self.next == 0 else 0
         self.check()
 
     def full(self):
-        for i in self.grid:
-            if "." in i: return False
-        self.winner = "tie"
+        if -1 in self.grid: return False
+        self.winner = -1
         return True
 
     def check3(self,x,y,z):
-        if x != "." and x == y and y == z: 
+        if x != -1 and x == y and y == z: 
             self.winner = x
             return True
         return False
     def check(self): 
-        if self.full(): return
-        for i in self.grid:
-            if self.check3(i[0],i[1],i[2]): return
         for i in range(3):
-            if self.check3(self.grid[0][i],self.grid[1][i],self.grid[2][i]): return
-        if self.check3(self.grid[0][0],self.grid[1][1],self.grid[2][2]): return
-        if self.check3(self.grid[0][2],self.grid[1][1],self.grid[2][0]): return
+            if self.check3(self.grid[i * 3],self.grid[i * 3 + 1],self.grid[i * 3 + 2]): return
+            if self.check3(self.grid[i],self.grid[3+i], self.grid[6+i]): return
+        if self.check3(self.grid[0], self.grid[4], self.grid[8]): return
+        if self.check3(self.grid[2],self.grid[4],self.grid[6]): return
+        if self.full(): return
 
-    def save(self):
-        idString = str(self.id)
-        file = open("files/"+idString,"wb")
-        pickle.dump(self.grid,file)
+    def save2(self, filename):
+        file = open("files/"+filename+".txt","a+")
+        file.write("{} {}\n".format(" ".join(map(str,self.grid)),str( self.winner)))
         file.close()
 
     def __str__(self):
-        return "\n----------\n".join([ " | ".join(i) for i in self.grid ])  
+        return "\n----------\n".join([ " | ".join( map(str,self.grid[i*3: i*3+3])) for i in range(3) ])  
          
     
--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from classicGrid import classicGrid
 
 if __name__ == "__main__":
     a = classicGrid()
-    while not a.winner:
+    while a.winner == None:
         print(a)
-        cmd = input("Put \""+a.next+"\" at: ")
+        cmd = input("Put \""+str(a.next)+"\" at: ")
         cmd = [int (i)  for i in cmd.strip().split()]
         if len(cmd) != 2: continue
         a.put(cmd[0],cmd[1])  
-    if a.winner != "tie":
-        print(a.winner,"wins! Saving as", a.id)
+    if a.winner != -1:
+        print(a.winner,"wins!")
     else: 
         print("Game tied.")
-    a.save()
+    a.save2("save1")
 
 
     # a = classicGrid(id = "d3a8f31d-ae9e-471e-8b6a-fa5880333d3f")
