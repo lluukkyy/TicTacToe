db80abaa6ae3e97d14e8e23699d8deea6e1f576e
Haoyun Zhu <zhuh12@rpi.edu>
2019-05-20 14:37:59 -0400

    .

--- main.py
+++ main.py
@@ -4,18 +4,27 @@ import random
 from classicGrid import classicGrid
 
 if __name__ == "__main__":
-    count = 0
-    while count <=1000:
-        count+=1
-        a = classicGrid()
-        while a.winner == None:
-            a.put(0,random.randint(0,8))  
+    # count = 0
+    # while count <=1000:
+    #     count+=1
+    #     a = classicGrid()
+    #     while a.winner == None:
+    #         a.put(0,random.randint(0,8))  
+    #     if a.winner != -1:
+    #         print(count,":",a.winner,"wins!")
+    #     else: 
+    #         print(count,":","Game tied.")
+    #     a.save2("save1")
+
+    a = classicGrid()
+    while a.winner == None:
+        print(a)
+        ipt = input("Put {} at:".format(str(a.next)))
+        row, col = ipt.strip().split()
+        a.put(int(row),int(col))  
         if a.winner != -1:
-            print(count,":",a.winner,"wins!")
+            print(a.winner,"wins!")
         else: 
-            print(count,":","Game tied.")
-        a.save2("save1")
+            print("Game tied.")
+        a.save2("save2")
 
-
-    # a = classicGrid(id = "d3a8f31d-ae9e-471e-8b6a-fa5880333d3f")
-    # print(a)
